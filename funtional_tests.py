15ea70249d421abb297460f43f9cc960976a2d47
TinHLQNguyen <tinnguyenhuulequang@gmail.com>
2023-10-16 07:31:16 +0900

    add a user story

--- funtional_tests.py
+++ funtional_tests.py
@@ -1,9 +1,28 @@
 from selenium import webdriver
 
-# Given a default runninng browser
+# Given a browser of choice like Firefox
 browser = webdriver.Firefox()
+
+# Edith has heard about a cool new online to-do app.
+# She goes to check out its homepage
 browser.get("http://localhost:8000")
 
-# The title of the default page should be "Congratulations"
-assert("Congratulations!" in browser.title)
-print("OK")
+# She notices the page title and header mention to-do lists
+assert("To-Do" in browser.title)
+
+# She is invited to enter a to-do item straight away
+
+# She types "Buy peacock feathers" into a text box
+# (Edith's hobby is tying fly-fishing lures)
+
+# When she hits enter, the page updates, and now the page lists
+# "1: Buy peacock feathers" as an item in a to-do list
+
+# There is still a text box inviting her to add another item.
+# She enters "Use peacock feathers to make a fly" (Edith is very methodical)
+
+# The page updates again, and now shows both items on her list
+
+# Satisfied, she goes back to sleep
+
+browser.quit()
